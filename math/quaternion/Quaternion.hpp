8a4e2ce1f1782fb4e6db998f4b6645f53765496e
Rody Oldenhuis <oldenhuis@gmail.com>
2014-05-16 15:17:41 +0200

    Added quaternion stub; nothing to see yet.

---
+++ math/quaternion/Quaternion.hpp
@@ -0,0 +1,14 @@
+#ifndef __QUATERNION_HPP
+#define __QUATERNION_HPP
+
+namespace math { 
+	
+class Quaternion 
+	: public Matrix<double,4,1>
+{
+	// just operator overloading
+};
+
+} // namespace math
+
+#endif
